97b0a8b3654482208b3b795239eaf70e2f59539c
debadritasaha7 <dbdtjosaha07@gmail.com>
2024-07-26 18:23:43 +0530

    create a sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import {signin, signup } from '../controllers/auth.controller.js';
 
 // The express.Router() function is used to create a new router object. This function is used when you want to create a new router object in your program to handle requests. 
 
 const router=express.Router();
 
 router.post("/signup",signup);
+router.post("/signin",signin);
 
 export default router;
