e784c6d7d74985173a5cb3dcefce795028a6fa1c
debadritasaha7 <dbdtjosaha07@gmail.com>
2024-08-13 01:16:39 +0530

    complete create listing page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -132,6 +132,11 @@ const handleSignOut=async()=>{
      
     <input type="password" placeholder="password" id='password' className="border p-3 rounded-lg" onChange={handleChange}/>
      <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'> {loading ? 'Loading...' : 'Update'}</button>
+     
+     <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'to={"/create-listing"}>
+      Create Listing 
+     </Link>
+     
      </form>
 
      <div className="flex justify-between mt-5">
